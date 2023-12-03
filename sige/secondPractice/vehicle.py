2393affa834dec7879dfbc6a0c884cd872a9d677
SiGe11 <sim.gergely@gmail.com>
2023-12-03 22:32:09 +0100

    SiGe Add missing description

--- sige/secondPractice/vehicle.py
+++ sige/secondPractice/vehicle.py
@@ -1,3 +1,8 @@
+# A közútkezelő megbízásából forgalomszámlálást végez az egyik forgalmas útszakaszon.
+# Kérje be konzolon, hogy hány kamion haladt el az elmúlt órában! Azt is, hogy hány személyautó!
+# Összesen hány egyéb jármű haladt át az útszakaszon, ha tudjuk, hogy abban az órában 250 jármű haladt át?
+# Írassa ki a látott járműveket egy sorban, "/" jellel elválasztva, az elvárt kimenet szerint!
+
 kamion = int(input())
 auto = int(input())
 
